3c377433a741ae4b1ee363de656128058a110980
Ocean <NAVY@thes-mbp.myfiosgateway.com>
2023-02-14 11:28:20 -0500

    Nawid

--- Review.ts
+++ Review.ts
@@ -2,7 +2,7 @@
 // let num:number = 39;
 // let bol:boolean = true;
 
-import { createImportSpecifier } from "typescript";
+import { createBigIntLiteral, createImportSpecifier } from "typescript";
 
 // import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
@@ -964,17 +964,86 @@ import { createImportSpecifier } from "typescript";
 // }
 
 
-function disemvowel(str:string){
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (char !== 'a' && char !== 'e' && char !== 'i' && char !== 'o' && char !== 'u' &&
-      char !== 'A' && char !== 'E' && char !== 'I' && char !== 'O' && char !== 'U') {
-      result += char;
-    }
+// function disemvowel(str:string){
+//   let result = "";
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (char !== 'a' && char !== 'e' && char !== 'i' && char !== 'o' && char !== 'u' &&
+//       char !== 'A' && char !== 'E' && char !== 'I' && char !== 'O' && char !== 'U') {
+//       result += char;
+//     }
+//   }
+//   return result;
+// }
+
+
+// console.log(disemvowel("This website is for losers LOL!"))
+
+
+// function toJadenCase(str:string){
+//   let result = str[0].toUpperCase();
+  
+//   for (let i = 0; i < str.length; i++){
+//       if(str[i-1] === " "){
+//         result += str[i].toUpperCase();
+//       }else {
+//         result += str[i].toLowerCase();
+//       }
+//   }
+
+//   return result;
+// };
+
+// console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
+
+
+// function lastSurvivor(letters:string, coords:number[]){
+//     let newString = "";
+//     let stringIndex = 0;
+
+//     for(let i = 0; i <letters.length; i++){
+//         if (stringIndex === coords[0]){
+//           coords.shift(
+//           )
+//         }else {
+//           newString+= letters[i]
+//           stringIndex++
+//         }
+//     }
+
+//     return newString;
+  
+// }
+
+// console.log(lastSurvivor('abc', [1, 1]));
+
+// console.log(lastSurvivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr',
+//  [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18,
+//  74, 59, 51, 47, 75, 49, 23, 56,  1, 33, 39, 33,
+//  34, 44, 25,  0, 51, 25, 36, 32, 57, 10, 57, 12,
+//  51, 55, 24, 55, 31, 49,  6, 15, 10, 48, 27, 29,
+//  38, 30, 35, 42, 23, 32,  9, 39, 39, 36,  8, 29,
+//   2, 33, 14,  3, 13, 25,  9, 25, 18, 10,  1,  2,
+//  20,  8,  2, 11,  5,  7,  0, 10, 10,  8, 12,  3,
+//   5,  1,  7,  7,  5,  1,  4,  0,  4,  0,  0,  1
+// ]));
+
+
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor (c:string, d:string, a:number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
   }
-  return result;
+
 }
 
+const invAbbas = new Invoice ("Abbas", "Finished Auth Service", 500);
+const invHakima = new Invoice ("Hakima", "Finished front end data picker, including sign up with G", 500)
 
-console.log(disemvowel("This website is for losers LOL!"))
+
+console.log(invAbbas, invHakima)
